63f4270b4cae850055101fe55e91489c3ecf0f2a
EclipseAltair <eclipsealtair@yandex.ru>
2022-08-22 15:35:08 +0300

    INFRA-1151: поправил переменные

--- devops/main.py
+++ devops/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 from app import config
 from app.routers import api_router
 
-app = FastAPI(title=config.ENVIRONMENT)
+app = FastAPI(title=config.ENVIRONMENT_FROM_SECRET)
 
 
 @app.get("/")
 async def root():
-    return {"message": f"Hello World {config.ENVIRONMENT_FROM_SECRET}!"}
+    return {"message": f"Hello World {config.ENVIRONMENT}!"}
 
 app.include_router(api_router)
